f7a5ba343b6bca08a4f3627b9feb0a18d0aab9f9
Binhd1305 <122051855+Binhd1305@users.noreply.github.com>
2024-04-19 14:09:38 -0500

    Update television.py

--- television.py
+++ television.py
@@ -1,50 +1,60 @@
+class Television:
+    # Class Constants
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
 
-from television import *
-def main():
-    # Television 1
-    tv_1 = Television()
-    tv_1.power()
-    print(tv_1) # Power = True, Channel = 0, Volume = 0
-    tv_1.channel_up()
-    tv_1.channel_up()
-    tv_1.volume_up()
-    print(tv_1) # Power = True, Channel = 2, Volume = 1
-    tv_1.channel_up()
-    tv_1.channel_up()
-    tv_1.channel_up()
-    tv_1.volume_down()
-    tv_1.volume_down()
-    print(tv_1) # Power = True, Channel = 1, Volume = 0
-    tv_1.power()
-    tv_1.volume_up()
-    tv_1.channel_down()
-    print(tv_1) # Power = False, Channel = 1, Volume = 0
-    tv_1.power()
-    tv_1.volume_up()
-    tv_1.volume_up()
-    tv_1.volume_up()
-    tv_1.channel_down()
-    tv_1.channel_down()
-    print(tv_1) # Power = True, Channel = 3, Volume = 2
-    # Television 2
-    tv_2 = Television()
-    tv_2.power()
-    tv_2.channel_up()
-    tv_2.volume_up()
-    print(tv_2) # Power = True, Channel = 1, Volume = 1
-    # Muting effect
-    tv_1.mute()
-    print(tv_1) # Power = True, Channel = 3, Volume = 0
-    tv_1.volume_down()
-    print(tv_1) # Power = True, Channel = 3, Volume = 1
-    tv_1.mute()
-    print(tv_1) # Power = True, Channel = 3, Volume = 0
-    tv_1.volume_up()
-    print(tv_1) # Power = True, Channel = 3, Volume = 2
-    tv_1.power()
-    tv_1.mute()
-    print(tv_1) # Power = False, Channel = 3, Volume = 2
+    def __init__(self) -> None:
+        """ This used to set the default instance variables."""
+        self.__mute: bool = False
+        self.__volume = Television.MIN_VOLUME
+        self.__status: bool = False
+        self.__channel = Television.MIN_CHANNEL
 
+    def power(self) -> None:
+        """This used to turn the tv on and off by changing the value of the status variable"""
+        self.__status = not self.__status
 
-if __name__ == '__main__':
-    main()
+    def mute(self) -> None:
+        """Mute or unmute the TV when it's on by changing the value of the muted variable."""
+        if self.__status:
+            self.__mute = not self.__mute
+
+    def channel_down(self) -> None:
+        """Decrease the TV channel value when the TV is on."""
+        if self.__status:
+            if self.__channel > Television.MIN_CHANNEL:
+                self.__channel -= 1
+            else:
+                self.__channel = Television.MAX_CHANNEL
+
+    def channel_up(self) -> None:
+        """Increase the TV channel value when the TV is on."""
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
+
+    def volume_up(self) -> None:
+        """Increase the TV volume when the TV is on."""
+        if self.__status:
+            self.__mute = False
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume += 1
+
+    def volume_down(self) -> None:
+        """Decrease the TV volume when the TV is on. If the TV is muted, unmute it."""
+        if self.__status:
+            self.__mute = False
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
+
+    def __str__(self) -> str:
+        """Send the values of the TV object in the format Power = [status], Channel = [channel], Volume = [volume].
+        :return the status, channel value, and volume value """
+        if self.__mute:
+            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}"
+        else:
+            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
